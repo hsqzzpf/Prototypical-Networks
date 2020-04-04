5cb68dc4b1f66656564f1d3f151e24fbfcaf3327
keelia <xiaoyuetang2017@gmail.com>
2020-04-04 21:18:16 +0800

    Update protonet_resnet.py

--- protonet_resnet.py
+++ protonet_resnet.py
@@ -20,7 +20,7 @@ class ProtoResNet(nn.Module):
     def __init__(self, x_dim=1, hid_dim=64, z_dim=64):
         super(ProtoResNet, self).__init__()
 
-        model_ft = models.resnet18(pretrained=True)
+        model_ft = models.resnet50(pretrained=True)
         model_ft.conv1=nn.Conv2d(x_dim, hid_dim, kernel_size=(3, 3), bias=False)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, z_dim)
